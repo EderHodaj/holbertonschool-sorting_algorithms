9da7fef9759ac187f68cf65d5d82af36a2f825f7
a7il10 <atilio.sulovari199@gmail.com>
2024-01-10 12:39:30 +0000

    betty issue

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,19 +9,19 @@
 
 void bubble_sort(int *array, size_t size)
 {
-    long unsigned int i, j, temp;
+	long unsigned int i, j, temp;
 
-    for (i = 0; i < size; i++)
-    {
-        for (j = 0; j < size - 1; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-                print_array(array, size);
-            }
-        }
-    }
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
 }
